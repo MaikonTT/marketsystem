b5c5836f42c37258c4d9cb776317b2986c0e3d9c
MaikonTT <maikon_numb@hotmail.com>
2019-08-16 17:27:05 -0300

    Update Funcionario.cs

--- marketsystem/marketsystem/classes/Funcionario.cs
+++ marketsystem/marketsystem/classes/Funcionario.cs
@@ -8,24 +8,24 @@ namespace marketsystem.classes
 {
     class Funcionario
     {
-        public int id_func { set; get; }
-        public string nome { set; get; }
-        public string cargo { set; get; }
-        public string endereco { set; get; }
-        public string telefone { set; get; }
-        public string data_nascimento { set; get }
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cargo { get; set; }
+        public string endereco { get; set; }
+        public string telefone { get; set; }
+        public string data_nasc { get; set; }
 
         public Funcionario() {
         }
 
         public Funcionario(int i, string n, string c, string e, string t, string d)
         {
-            id_func = i;
+            id = i;
             nome = n;
             cargo = c;
             endereco = e;
             telefone = t;
-            data_nascimento = d;
+            data_nasc = d;
         }
     }
 }
