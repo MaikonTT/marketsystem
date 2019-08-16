722708a9326a3e3c010b2374f80c1e41d8c66f9c
MaikonTT <maikon_numb@hotmail.com>
2019-08-16 17:27:12 -0300

    Update Marca.cs

--- marketsystem/marketsystem/classes/Marca.cs
+++ marketsystem/marketsystem/classes/Marca.cs
@@ -8,16 +8,16 @@ namespace marketsystem.classes
 {
     class Marca
     {
-        public int id_marca { set; get; }
-        public string nome { set; get; }
-        public string endereco { set; get; }
-        public string telefone { set; get; }
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string endereco { get; set; }
+        public string telefone { get; set; }
 
         public Marca() {
         }
 
         public Marca(int i, string n, string e, string t) {
-            id_marca = i;
+            id = i;
             nome = n;
             endereco = e;
             telefone = t;
