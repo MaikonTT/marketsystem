39f70f84e4c698a50901f18ce327b79df863506b
MaikonTT <maikon_numb@hotmail.com>
2019-08-16 17:27:56 -0300

    Create Conexao.cs

---
+++ marketsystem/marketsystem/util/Conexao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace marketsystem.util
+{
+    class Conexao
+    {
+        public NpgsqlConnection CriarConexao()
+        {
+            //string de conexão
+            string strConexao = string.Format("Server={0}; Port={1}; User Id={2}; Password={3}; Database={4}",
+                                              "localhostl", "5432", "postgres", "postgres", "dbmarket");
+
+            //abre conexão com o banco
+            NpgsqlConnection conexao = new NpgsqlConnection();
+            conexao.Open();
+
+            //retorna conexão;
+            return conexao;
+        }
+    }
+}
