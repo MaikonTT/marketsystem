201fdcc42a1a4958f225bba72cfd66ea80b7f20d
MaikonTT <maikon_numb@hotmail.com>
2019-08-18 15:51:06 -0300

    Update TelaFuncionario.cs

--- marketsystem/marketsystem/TelaFuncionario.cs
+++ marketsystem/marketsystem/TelaFuncionario.cs
@@ -19,11 +19,51 @@ namespace marketsystem
             InitializeComponent();
         }
 
-        private void btnListar_Click(object sender, EventArgs e)
+        private void BtnListar_Click(object sender, EventArgs e)
         {
             FuncionarioDAO fDAO = new FuncionarioDAO();
             List<Funcionario> dataDAO = fDAO.Listar();
             dgvFunc.DataSource = dataDAO;
+        }
+
+        private void BtnCadastrar_Click(object sender, EventArgs e)
+        {
+            //adiciona o input nas variaveis
+            string Nome = txtNome.Text;
+            string Cargo = txtCargo.Text;
+            string Endereco = txtEndereco.Text;
+            string Telefone = txtTelefone.Text;
+            string Data_nasc = txtData_nasc.Text;
+
+            //instancia modelo Funcionario
+            Funcionario f = new Funcionario();
+            FuncionarioDAO fDAO = new FuncionarioDAO();
+
+            try
+            {
+                //atribui texto do input nos atributos do modelo Funcionario
+                f.Nome = Nome;
+                f.Cargo = Cargo;
+                f.Endereco = Endereco;
+                f.Telefone = Telefone;
+                f.Data_nasc = Data_nasc;
+
+                fDAO.Cadastrar(f);
+
+                MessageBox.Show("Cadastrado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível cadastrar funcionario " + ex.Message);
+            }
+            finally
+            {
+                txtNome.Clear();
+                txtCargo.Clear();
+                txtData_nasc.Clear();
+                txtTelefone.Clear();
+                txtEndereco.Clear();
+            }
         }
     }
 }
